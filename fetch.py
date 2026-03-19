934c18df6d8d26ed25107ef60912e558857d7235
Jaime Diaz <Jdiaz651@outlook.com>
2026-03-19 01:45:43 -0400

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -1,9 +1,23 @@
 import argparse
 
+import requests
+from requests.sessions import Request
+
 from weather_icons import weather_icons
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-l", help="city name")
 args = parser.parse_args()
 
-print(weather_icons["sunny"])
+
+def get_coordinates(city):
+    api_requests = {"name": city}
+    data = requests.get(
+        "https://geocoding-api.open-meteo.com/v1/search", params=api_requests
+    )
+    return data.json()
+
+
+result = get_coordinates(args.l)
+print(result["results"][0]["latitude"])
+print(result["results"][0]["longitude"])

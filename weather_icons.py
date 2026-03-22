caf5f06fed8ded085e494b81a78df18bd073a1c9
Jaime Diaz <Jdiaz651@outlook.com>
2026-03-22 01:49:47 -0400

    updated get weather function

--- weather_icons.py
+++ weather_icons.py
@@ -1,5 +1,17 @@
+def get_weather_icon(code):
+    match code:
+        case 0:
+            return "Sunny"
+        case 1 | 2 | 3:
+            return "Cloudy"
+        case 51 | 53 | 55 | 61 | 63 | 65 | 80 | 81 | 82:
+            return "Rainy"
+        case _:
+            return "Sunny"  # default fallback
+
+
 weather_icons = {
-    "sunny": """\
+    "Sunny": """\
     |
   \\ | /
    \\*/
@@ -8,7 +20,7 @@ weather_icons = {
   / | \\
     |
 """,
-    "cloudy": """\
+    "Cloudy": """\
   .--.
  (    )
 (______)

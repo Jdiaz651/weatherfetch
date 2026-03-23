7ad89ca784ef7fac0ca65201da4f3359786746d3
Jaime Diaz <Jdiaz651@outlook.com>
2026-03-22 20:57:02 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# config.py
+DEFAULT_UNIT = "imperial"
+DEFAULT_CITY = "miami"
+
+DISPLAY_INFO = [
+    "city",
+    "temperature",
+    "feels_like",
+    "humidity",
+    "wind_speed",
+    "precipitation",
+    "cloud_cover",
+    "uv_index",
+    "condition",
+]
